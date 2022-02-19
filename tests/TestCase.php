e4ee5fb3e273fd50f9aa950a0073383d0f9c2551
Henrique Ramos <henrique@henriqueramos.eti.br>
2022-02-19 03:15:03 -0300

    feat: Adding ArraySubsetAsserts trait on TestCase. To use the removed assertArraySubset as package

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,9 +1,13 @@
-<?php declare(strict_types = 1);
+<?php
 
-namespace Tests;
+declare(strict_types=1);
 
+namespace HenriqueRamosTests;
+
+use DMS\PHPUnitExtensions\ArraySubset\ArraySubsetAsserts;
 use PHPUnit\Framework\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
+    use ArraySubsetAsserts;
 }
