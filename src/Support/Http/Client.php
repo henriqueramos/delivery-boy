453b048ec883e603fd6ea5861d3c6a20bab5d258
Henrique Ramos <henrique@henriqueramos.eti.br>
2022-02-21 06:23:27 -0300

    fix: Adjust Http Client to handle requests and responses with graceful exit

--- src/Support/Http/Client.php
+++ src/Support/Http/Client.php
@@ -5,20 +5,29 @@ declare(strict_types=1);
 namespace HenriqueRamos\DeliveryBoy\Support\Http;
 
 use CurlHandle;
+use HenriqueRamos\DeliveryBoy\Support\Env;
+use InvalidArgumentException;
+use RuntimeException;
 
 class Client
 {
+    public const HTTP_OK = 200;
+
     protected $url = null;
 
-    public function __construct(
-        string $url
-    ) {
-        $this->url = $url;
+    public function __construct()
+    {
+        if (($apiUri = Env::string('API_URL')) === '') {
+            throw new InvalidArgumentException('fill.API_URL.environment.variable');
+        }
+
+        $this->url = $apiUri;
     }
 
     public function prepareRequest(): CurlHandle
     {
         $request = curl_init();
+        curl_setopt($request, CURLOPT_URL, $this->url);
         curl_setopt(
             $request,
             CURLOPT_HTTPHEADER,
@@ -31,12 +40,33 @@ class Client
         return $request;
     }
 
-    public function doRequest(CurlHandle $request): array
+    public function doPost(string $payload): string
     {
-        $error = curl_error($request);
-        $httpCode = curl_getinfo($request, CURLINFO_HTTP_CODE);
-        $output = curl_exec($request);
+        $request = $this->prepareRequest();
 
-        return [];
+        curl_setopt($request, CURLOPT_CUSTOMREQUEST, 'POST');
+        curl_setopt($request, CURLOPT_POSTFIELDS, $payload);
+
+        return $this->doRequest($request);
+    }
+
+    public function doRequest(CurlHandle $request): string
+    {
+        $output = curl_exec($request);
+        $httpCode = curl_getinfo($request, CURLINFO_HTTP_CODE);
+        $error = curl_error($request);
+
+        if ($httpCode !== self::HTTP_OK) {
+            $message = 'Ouch! We got some wrong response from 3rd API: ' . PHP_EOL .
+                'Error: ' . var_export($error, true) . PHP_EOL .
+                'Http Code: ' . var_export($httpCode, true) . PHP_EOL .
+                'Body: ' . var_export($output, true);
+
+            error_log($message);
+
+            throw new RuntimeException('api.response.differ.than.200.check.logs');
+        }
+
+        return $output;
     }
 }
